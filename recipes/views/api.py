6a13c4209a63d5bcdd542441a753de919fd2e377
Renan Matias <rnmatias@hotmail.com>
2023-02-08 20:01:29 -0300

    292. Entendendo validate e validate_field + ValidationError no Django Rest Framework

--- recipes/views/api.py
+++ recipes/views/api.py
@@ -26,7 +26,7 @@ def recipe_api_list(request):
             data=request.data
         )
         serializer.is_valid(raise_exception=True)
-        serializer.save()
+        # serializer.save()
         return Response(
             serializer.validated_data,
             status=status.HTTP_201_CREATED
